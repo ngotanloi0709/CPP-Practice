a177237e7ab1049cd17163ac84ff81b66a03e249
ngotanloi0709 <ngotanloi0709@gmail.com>
2021-08-07 17:42:10 +0700

    Update Bài tập  Đếm số lượng chữ số của N
    bài này khúc đầu suy nghĩ quá trời, rối 1 lùm, khúc  sau viết lại có mấy dòng :(

---
+++ DemSoluongChusoCuaN.cpp
@@ -0,0 +1,27 @@
+//Input: So nguyen N (00000)
+//Output: So luong chu so cua no (1)
+//https://luyencode.net/problem/VL10
+//Luu y: khong co truong hop so bat dau = 0
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void numberCounting(int n) {
+	int test = 1;
+	int count = 0;
+	
+	while (abs(n) / test >= 1) {
+		count = count + 1;
+		test = test * 10;
+	}
+	cout << "> "<< n << " la so co " << count << " chu so!!!";
+}
+
+int main() {
+	int n;
+	cout << "> Nhap so ban muon kiem tra: ";
+	cin >> n;
+	numberCounting(n);
+	return 0;
+}
+//bai nay roi vai :(( moi dau lam dung phuc tap, khuc sau lam lai ra dung ngan :(
